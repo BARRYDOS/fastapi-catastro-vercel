bc45fb6e1d4c573555c96e3d4a5bca3342a40005
carlos-amador-git <66793740+carlos-amador-git@users.noreply.github.com>
2025-11-28 12:14:29 -0600

    Delete api/main.py

--- api/main.py
+++
@@ -1,9 +0,0 @@
-"""
-Handler para Vercel - Este archivo es el punto de entrada
-Coloca este archivo en: api/main.py
-"""
-from mangum import Mangum
-from index import app
-
-# Mangum convierte FastAPI para que funcione en Vercel
-handler = Mangum(app)
